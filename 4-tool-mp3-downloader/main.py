1461b4745d4fa282d876f459e32ce06a8a34f26b
Michael Saccone <jason@MBPdiMichael2-2.lan>
2024-06-21 13:43:30 +0200

    Fix Youtube song finder

--- 4-tool-mp3-downloader/main.py
+++ 4-tool-mp3-downloader/main.py
@@ -5,33 +5,32 @@ import requests
 
 def search_youtube_video(video_title):
     # cerca il video
+    print ("Searching " + video_title)
     search_results = Search(video_title)
+
+    for v in search_results.results:
+        print(f"{v.title}\n{v.watch_url}\n")
+
     video = search_results.results[0]
 
     audio_stream = video.streams.filter(only_audio=True).first()
 
-    audio_url = audio_stream.url
-    return audio_url
+    return audio_stream
 
-@tool(examples=['download this song', 'scarica la canzone'])
+@tool(examples=['send me this song', 'scarica la canzone', 'mandami la canzone'], return_direct=True)
 def download_song_from_youtube(song_title, cat):
     """
-    Download a song or music or whatever audio from YouTube locally. 
+    Download a song or music or whatever audio from YouTube locally.
+    Sends a song to the Human.
     Input is ALWAYS the name of the song or video to search for
     """
-    audio_url = search_youtube_video(song_title)
+    audio = search_youtube_video(song_title)
     # scarica il file audio localmente (il file audio si andrà sempre a sovrascrivere)
-    audio_file = requests.get(audio_url)
-    audio_file_path = 'audio.mp3'
-    with open(audio_file_path, 'wb') as f:
-        f.write(audio_file.content)
+    out_file = audio.download(output_path='/app/cat/static') 
 
-@tool(examples=['give me the link', 'find the link of'], return_direct=True)
-def get_youtube_audio_link(song_title, cat):
-    """
-       Find the link to download a song or audio from YouTube.
-       Input is ALWAYS the title of the song or audio to download
-    """
-    audio_url = search_youtube_video(song_title)
-    # link per il download
-    return audio_url
+    print("FILE: " + out_file)
+
+    new_file = '/app/cat/static/audio.mp3'
+    os.rename(out_file, new_file)
+
+    return '<audio controls><source src="http://localhost:1865/static/audio.mp3" type="audio/mpeg"></audio>'
